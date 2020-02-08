ed8d3cca1dc63f4fe7a8c1787bdba983fc56d876
yzq838 <550279807@qq.com>
2020-02-08 22:11:35 +0800

    修改front下的category.js

--- web_front/js/util/category.js
+++ web_front/js/util/category.js
@@ -2,23 +2,35 @@
 
 var category = {
     // 获取
-    get:function(){
-        return $.get(APILIST.category_get)
-    },
+    // get:function(){
+    //     return $.get(APILIST.category_get)
+    // },
+    get: () => $.get(APILIST.category_get),
+
+
     /**
      * 添加文章类型
      * @param {*} name 
      * @param {*} slug 
      */
-    add:function(name,slug){
-        return $.post(APILIST.category_add,{'name':name ,'slug':slug })
-    },
+    // add:function(name,slug){
+    //     return $.post(APILIST.category_add,{'name':name ,'slug':slug })
+    // },
+    add: (name, slug) => $.post(APILIST.category_add, { name, slug }),
+
+
     // 删除文章类型
-    del:function(id){
-        return $.post(APILIST.category_del,{'id': id})
-    },
+    // del: function (id) {
+    //     return $.post(APILIST.category_del, { 'id': id })
+    // },
+    del: id => $.post(APILIST.category_del, { id }),
+
+
     // 编辑文章类型
-    edit:function(id,name,slug){
-        return $.post(APILIST.category_edit,{'id':id,'name':name ,'slug':slug })
-    }
+    // edit: function (id, name, slug) {
+    //     return $.post(APILIST.category_edit, { 'id': id, 'name': name, 'slug': slug })
+    // }
+    edit: (id, name, slug) => $.post(APILIST.category_edit, { id, name, slug })
+
+
 }
